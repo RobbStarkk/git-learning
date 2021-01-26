99058899fbc9019bcb236921437a6c32d08bb454
Виктор Урушкин <viktor@MacBook-Air-Viktor.local>
2021-01-26 19:55:34 +0700

    first commit

---
+++ boolean_function.py
@@ -0,0 +1,27 @@
+def bp(x1, x2):
+    if x1 > 0 and x2 > 0:
+        return 0
+    else:
+        return x1 + x2
+
+
+def mb(x):
+    b = ''
+    while x > 0:
+        b = str(x % 2) + b
+        x = x // 2
+    if b == '':
+        return 0
+    else:
+        return int(b)
+
+
+i = 0
+while i < 2 ** 4:
+    x1 = mb(i) // 1000
+    x2 = (mb(i) % 1000) // 100
+    x3 = (mb(i) % 100) // 10
+    x4 = mb(i) % 10
+    f = bp(x1, x4)
+    print(x1, x2, x3, x4, f, sep=' ')
+    i += 1
---
+++ log2.py
@@ -0,0 +1,9 @@
+import math as m
+
+def log2(x):
+	return (m.log(x) / m.log(2)) 
+
+print('Hi, I learning a git! This small program calculates the base 2 logarithm')
+print('Enter a number:')
+a = input()
+print(log2(a))
